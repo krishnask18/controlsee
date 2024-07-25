28588dd1cd2715a916b45722da6b7df6ab30630b
Krishna <shrikrishnas1812@gmail.com>
2024-07-25 16:35:28 +0530

    asdf

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import App from "./App";
 
 function Feed(){
+    async function fetchdata(){
+        const usr = await fetch('https://csbackend-git-main-krishnas-projects-e88a8c5b.vercel.app/feed', {
+            method : 'GET',
+            credentials : 'include',
+            mode : 'cors',
+        })
+        usr = await usr.json()
+        console.log(usr);
+    }
+    useEffect(()=>{
+        fetchdata()
+    }, [])
     return(
         <div>
             Feed

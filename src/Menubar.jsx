559c4dd295d6391b0c9e38bddab0021b22da0e62
Krishna <shrikrishnas1812@gmail.com>
2024-07-25 16:08:42 +0530

    dfsdf

--- src/Menubar.jsx
+++ src/Menubar.jsx
@@ -72,7 +72,7 @@ function Menubar(){
                             })
                             usr = await usr.json()
                             const name = usr['name']
-                            window.location.href = name ? '/profile/'+encodeURIComponent(name) : 'https://csbackend-git-main-krishnas-projects-e88a8c5b.vercel.app?rqst='+encodeURIComponent(window.location.href)
+                            window.location.href = name ? '/profile/'+encodeURIComponent(name) : 'https://csbackend-git-main-krishnas-projects-e88a8c5b.vercel.app?rqst='+encodeURIComponent('https://controlsee-git-main-krishnas-projects-e88a8c5b.vercel.app/')
                         }}
                         style={{
                             background : "#233731"
